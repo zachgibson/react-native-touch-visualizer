66d4c34aa441065ae1db6d88352f1f81cff14519
Zachary Gibson <zach.gibson@buzznog.com>
2016-12-20 17:45:46 -0600

    Initial commit

---
+++ TouchVisualizer.h
@@ -0,0 +1,24 @@
+#import "RCTBridgeModule.h"
+#include <UIKit/UIKit.h>
+
+@protocol TouchVisualizerDelegate;
+
+@interface TouchVisualizer : UIWindow <RCTBridgeModule>
+
+@property (nonatomic, weak) id<TouchVisualizerDelegate> touchVisualizerWindowDelegate;
+
+// Touch effects
+@property (nonatomic) UIImage *touchImage;
+@property (nonatomic) CGFloat touchAlpha;
+@property (nonatomic) NSTimeInterval fadeDuration;
+@property (nonatomic) UIColor *strokeColor;
+@property (nonatomic) UIColor *fillColor;
+
+// Ripple Effects
+@property (nonatomic) UIImage *rippleImage;
+@property (nonatomic) CGFloat rippleAlpha;
+@property (nonatomic) NSTimeInterval rippleFadeDuration;
+@property (nonatomic) UIColor *rippleStrokeColor;
+@property (nonatomic) UIColor *rippleFillColor;
+
+@end
